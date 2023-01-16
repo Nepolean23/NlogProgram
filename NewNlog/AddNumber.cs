f55ac926ea83bc3136e1e79b5a3998231dc6186c
Nepolean23 <nlean1996@gmail.com>
2023-01-16 14:46:25 +0530

    [NEPOLEAN]-Code for NlogProgram

---
+++ NewNlog/AddNumber.cs
@@ -0,0 +1,39 @@
+﻿using NLog.Fluent;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewNlog
+{
+    public class  AddNumber
+    {
+
+        Nlog nlog = new Nlog();
+
+        public int Sum(int a, int b)
+        {
+            if (a == 0 || b == 0)
+            {
+                nlog.LogDebug("Debug sucessfull : Sum()");
+                nlog.LogError("Expecting null variables");
+                nlog.LogWarn("a or b should nont be equal to 0");
+
+            }
+            int c = a + b;
+            nlog.LogDebug("Debug sucessfull");
+            nlog.LogInfo("Sum Method Passed" + c);
+            return c;
+
+        }
+
+
+            
+
+            
+
+        
+        
+    }
+}
---
+++ NewNlog/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace NewNlog
+{
+    public class Program
+    {
+        static void Main(string[] args) 
+        {
+            AddNumber addnumber = new AddNumber();
+            int result = addnumber.Sum(12, 13);
+            Console.WriteLine(result);
+
+        }
+    }
+}
+
